126499012f6d4f3cb151d93f9499aa4f60ac0ac1
Aleksey Shubin <alekseysshubin@gmail.com>
2014-12-30 17:03:13 +1000

    Sort server list by column header click.

--- WarbandServerMonitor/View/ServerList.xaml.cs
+++ WarbandServerMonitor/View/ServerList.xaml.cs
@@ -1,10 +1,38 @@
-﻿namespace WarbandServerMonitor.View
+﻿using System.ComponentModel;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+
+namespace WarbandServerMonitor.View
 {
     public partial class ServerList
     {
+        private GridViewColumnHeader _listViewSortCol;
+        private SortAdorner _listViewSortAdorner;
+
         public ServerList()
         {
             InitializeComponent();
+        }
+
+        private void ColumnHeaderOnClick(object sender, RoutedEventArgs e)
+        {
+            var column = (GridViewColumnHeader)sender;
+            var sortBy = column.Tag.ToString();
+            if (_listViewSortCol != null)
+            {
+                AdornerLayer.GetAdornerLayer(_listViewSortCol).Remove(_listViewSortAdorner);
+                ServersListView.Items.SortDescriptions.Clear();
+            }
+
+            var newDir = ListSortDirection.Ascending;
+            if (_listViewSortCol == column && _listViewSortAdorner.Direction == newDir)
+                newDir = ListSortDirection.Descending;
+
+            _listViewSortCol = column;
+            _listViewSortAdorner = new SortAdorner(_listViewSortCol, newDir);
+            AdornerLayer.GetAdornerLayer(_listViewSortCol).Add(_listViewSortAdorner);
+            ServersListView.Items.SortDescriptions.Add(new SortDescription(sortBy, newDir));
         }
     }
 }
---
+++ WarbandServerMonitor/View/SortAdorner.cs
@@ -0,0 +1,43 @@
+﻿using System.ComponentModel;
+using System.Windows;
+using System.Windows.Documents;
+using System.Windows.Media;
+
+namespace WarbandServerMonitor.View
+{
+    public class SortAdorner : Adorner
+    {
+        private static readonly Geometry _ascGeometry = Geometry.Parse("M 0 4 L 3.5 0 L 7 4 Z");
+
+        private static readonly Geometry _descGeometry = Geometry.Parse("M 0 0 L 3.5 4 L 7 0 Z");
+
+        public ListSortDirection Direction { get; private set; }
+
+        public SortAdorner(UIElement element, ListSortDirection dir)
+            : base(element)
+        {
+            Direction = dir;
+        }
+
+        protected override void OnRender(DrawingContext drawingContext)
+        {
+            base.OnRender(drawingContext);
+
+            if (AdornedElement.RenderSize.Width < 20) return;
+
+            var transform = new TranslateTransform
+                (
+                    AdornedElement.RenderSize.Width - 10,
+                    (AdornedElement.RenderSize.Height - 5) / 2
+                );
+            drawingContext.PushTransform(transform);
+
+            var geometry = _ascGeometry;
+            if (Direction == ListSortDirection.Descending)
+                geometry = _descGeometry;
+            drawingContext.DrawGeometry(Brushes.Black, null, geometry);
+
+            drawingContext.Pop();
+        }
+    }
+}
