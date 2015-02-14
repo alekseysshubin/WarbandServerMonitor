25977a2d79b0cb3e8a89d6a7662bd796caca198f
Aleksey Shubin <alekseysshubin@gmail.com>
2015-02-14 20:24:31 +1000

    Pinger - version with single UdpClient (unfinished).

---
+++ WarbandServerMonitor/Model/PingRequest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Diagnostics;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace WarbandServerMonitor.Model
+{
+    public class PingRequest
+    {
+        public IPEndPoint EndPoint { get; set; }
+        public TaskCompletionSource<long> TaskCompletionSource { get; set; }
+        public Stopwatch Stopwatch { get; set; }
+        public Int16 Token { get; set; }
+    }
+}
---
+++ WarbandServerMonitor/Model/Pinger.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections.Concurrent;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+
+namespace WarbandServerMonitor.Model
+{
+    public class Pinger
+    {
+        // There is no ConcurrentCollection or ConcurrentList in .NET, so have to use ConcurrentDictionary.
+        private readonly ConcurrentDictionary<PingRequest, PingRequest> _requests;
+
+        private readonly UdpClient _client;
+        private bool _stop;
+
+        public Pinger()
+        {
+            _client = new UdpClient();
+            _requests = new ConcurrentDictionary<PingRequest, PingRequest>();
+            Receiver();
+        }
+
+        public Task<long> Ping(IPAddress ip, int port)
+        {
+            var request = new PingRequest
+            {
+                EndPoint = new IPEndPoint(ip, port),
+                TaskCompletionSource = new TaskCompletionSource<long>()
+            };
+            _requests.TryAdd(request, request);
+            return request.TaskCompletionSource.Task;
+        }
+
+        private async void Receiver()
+        {
+            while (true)
+            {
+                if (_stop) return;
+
+                var response = await _client.ReceiveAsync();
+                if (response.Buffer.Length != 6) continue;
+                var token = (response.Buffer[2] << 8) + response.Buffer[1];
+                var request = _requests.FirstOrDefault(x => x.Value.Token == token).Value;
+                if (request == null) continue;
+
+                request.Stopwatch.Stop();
+                _requests.TryRemove(request, out request);
+                request.TaskCompletionSource.SetResult(request.Stopwatch.ElapsedMilliseconds);
+            }
+        }
+
+        private void SendPings()
+        {
+            while (!_requests.IsEmpty)
+            {
+                
+            }
+        }
+    }
+}
